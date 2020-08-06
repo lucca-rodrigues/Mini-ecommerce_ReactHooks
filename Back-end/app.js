dec7f45276c4847ddfc3a724a42c10f70d4f37c0
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-08-05 21:47:59 -0300

    API Node

--- Back-end/app.js
+++ Back-end/app.js
@@ -1,6 +1,24 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.listen(3333);
+const {finalizarCompra, obterCidadesPorEstados} = require('');
+
+const app = express();
+const port = 3333;
+
+// CONFIG
+app.use(express.static('public'));
+app.use(cors());
+app.use(bodyParser.json());
+
+
+// MÉTODOS HTTP
+app.post('ecommerce/checkout/finalizar-compra', finalizarCompra);
+
+app.get('ecommerce/estado/:siglaEstado/cidades', obterCidadesPorEstados);
+//
+
+app.listen(port, () => console.log(`Servidor inicializado na porta ${port}`));
 
 module.exports = app;
