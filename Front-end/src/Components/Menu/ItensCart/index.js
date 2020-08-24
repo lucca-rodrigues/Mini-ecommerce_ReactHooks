ef2a3ac6f74592a281aca04e589c4574df53dfbe
lucca-rodrigues <contato.luccarodrigues@gmail.com>
2020-08-24 12:26:59 -0300

    list products to cart

--- Front-end/src/Components/Menu/ItensCart/index.js
+++ Front-end/src/Components/Menu/ItensCart/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import {FaSadTear} from 'react-icons/fa';
 
 // import { Container } from './styles';
 
-function ItensCart() {
+function ItensCart(props) {
+  function renderProdutocts(){
+    // Se carrinho estiver vazio
+    if(props.produtos.length === 0){
+      return (
+        <NavDropdown.Item href="" data-testid="itens">
+          <FaSadTear size={20} />
+          Carrinho vazio...
+        </NavDropdown.Item>
+      )
+    }
+  }
   return <div />;
 }
-
+ItensCart.propTypes = {
+  produtos: PropTypes.array.isRequired
+}
 export default ItensCart;
